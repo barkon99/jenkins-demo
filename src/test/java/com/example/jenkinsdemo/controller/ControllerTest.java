da228f6ed350920c4327a05f4938d47528eb3fea
Bartek <bartekkonewka@gmail.com>
2022-04-20 18:41:56 +0200

    change test

--- src/test/java/com/example/jenkinsdemo/controller/ControllerTest.java
+++ src/test/java/com/example/jenkinsdemo/controller/ControllerTest.java
@@ -2,6 +2,8 @@ package com.example.jenkinsdemo.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +11,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 class ControllerTest {
 
     @Autowired
